af6692aff41c48096232b77b79d0e47c1bd7ca53
bycold1 <114782495+bycold1@users.noreply.github.com>
2022-11-24 10:48:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import moment from 'moment';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "629c9395db137563243ef607612b9511";
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState("Toronto");
+
+
+  useEffect(() => {
+      getCityWeather();
+  }, []);
+
+  const getCityWeather=()=> {
+    axios.get(`${API_URL}?q=${city}&appid=${API_KEY}`).then((response) => {
+      setData(response.data);
+      console.log(response.data)
+    });
+  }
+
+const searchCity=(e)=> {
+setCity(e.target.value);
+
+ getCityWeather();
+}
+
+return (
+  <div className="App">
+    {!data ? (
+      <div>Loading ...</div>
+    ) : (
+      <>
+        <div className="leftContent">
+          <h4>101202014 comp3123_labtest2 </h4>
+          <div className="information">
+            <div className="celcius">
+                <h1>
+                 Current Temp:  {(data.main.temp - 273.15).toFixed(1)} 
+                  <sup>&#176;</sup>
+                </h1>
+              </div>
+              <div className="city">
+                <h2>{data.name}</h2>
+                <p>{moment().format("dddd MMM Do YY")}</p>
+              </div>
+              <div className="icon">
+                <img
+                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="rightContent">
+          <input type="text" className="search-bar" value={city} onChange={(e)=> searchCity(e)}/><div className="search"></div>
+            <div className="details">
+              <h3>Weather Details</h3>
+
+              <div className="detailItem">
+                <div className="left">
+                <span>Cloudy: </span>  <span className="right"> {data.clouds.all} %</span>
+              </div>
+               
+              </div>
+
+              <div className="detailItem">
+                <div className="left">
+                <span>Humidity: </span>  <span className="right">
+                {data.main.humidity} %
+                </span>
+                </div>
+          
+               
+              </div>
+              <div className="detailItem">
+                <div className="left">
+                <span>Wind:</span>  <span className="right">{data.wind.speed} Km/h</span>
+              </div>
+           
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
